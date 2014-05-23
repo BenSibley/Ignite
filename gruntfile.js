1d25caa2f5ac891eda70475af209aafec040b294
BenSibley <ben@competethemes.com>
2014-05-22 21:13:16 -0400

    * added grunt

---
+++ gruntfile.js
@@ -0,0 +1,77 @@
+module.exports = function(grunt) {
+
+    // 1. All configuration goes here
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        concat: {
+            dist: {
+                src: [
+                    'js/*.js'
+                ],
+                dest: 'js/build/production.js'
+            }
+        },
+        uglify: {
+            build: {
+                src: 'js/build/production.js',
+                dest: 'js/build/production.min.js'
+            }
+        },
+        watch: {
+            scripts: {
+                files: ['js/*.js'],
+                tasks: ['concat', 'uglify'],
+                options: {
+                    spawn: false
+                }
+            },
+            css: {
+                files: ['sass/*.scss'],
+                tasks: ['sass', 'autoprefixer', 'cssmin'],
+                options: {
+                    livereload: true,
+                    spawn: false
+                }
+            }
+        },
+        sass: {
+            dist: {
+                options: {
+                    style: 'expanded'
+                },
+                files: {
+                    'style.css': 'sass/style.scss'
+                }
+            }
+        },
+        autoprefixer: {
+            dist: {
+                options: {
+                    browsers: ['last 1 version', '> 1%', 'ie 8']
+                },
+                src: 'style.css',
+                dest: 'style-prefixed.css'
+            }
+        },
+        cssmin: {
+            combine: {
+                files: {
+                    'style.min.css': ['style-prefixed.css']
+                }
+            }
+        }
+    });
+
+    // 3. Where we tell Grunt we plan to use this plug-in.
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+    // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
+    grunt.registerTask('default', ['concat', 'uglify', 'watch', 'sass', 'autoprefixer', 'cssmin']);
+
+};
