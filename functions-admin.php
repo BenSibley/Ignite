f98849aad2d9be910613b66cab3ac02f55940e56
BenSibley <ben@competethemes.com>
2014-07-27 16:52:50 -0400

    marked functions-admin strings for translation and updated theme options page copy

--- functions-admin.php
+++ functions-admin.php
@@ -14,7 +14,7 @@ function ct_ignite_customize_register_logo( $wp_customize ) {
 	$wp_customize->add_section(
 		'ct-ignite-upload',
 		array(
-			'title'      => esc_html__( 'Logo', 'ignite' ),
+			'title'      => __( 'Logo', 'ignite' ),
 			'priority'   => 30,
 			'capability' => 'edit_theme_options'
 		)
@@ -28,7 +28,6 @@ function ct_ignite_customize_register_logo( $wp_customize ) {
 			'type'              => 'theme_mod',
 			'capability'        => 'edit_theme_options',
 			'sanitize_callback' => 'esc_url_raw',
-			//'transport'         => 'postMessage'
 		)
 	);
 
@@ -36,7 +35,7 @@ function ct_ignite_customize_register_logo( $wp_customize ) {
 		new WP_Customize_Image_Control(
 			$wp_customize, 'logo_image',
 				array(
-					'label'    => esc_html__( 'Upload custom logo.', 'ignite' ),
+					'label'    => __( 'Upload custom logo.', 'ignite' ),
 					'section'  => 'ct-ignite-upload',
 					'settings' => 'logo_upload',
 			)
@@ -64,7 +63,7 @@ function ct_ignite_customize_logo_positioning( $wp_customize ) {
     $wp_customize->add_section(
         'ct-logo-positioning',
         array(
-            'title'      => esc_html__( 'Logo Positioning', 'ignite' ),
+            'title'      => __( 'Logo Positioning', 'ignite' ),
             'priority'   => 31,
             'capability' => 'edit_theme_options'
         )
@@ -90,7 +89,7 @@ function ct_ignite_customize_logo_positioning( $wp_customize ) {
         new ct_ignite_number_input_control(
             $wp_customize, 'logo_positioning_updown_setting',
             array(
-                'label' => 'Up/down',
+                'label' => __('Up/down', 'ignite'),
                 'section' => 'ct-logo-positioning',
                 'settings' => 'logo_positioning_updown_setting',
                 'type' => 'number',
@@ -102,7 +101,7 @@ function ct_ignite_customize_logo_positioning( $wp_customize ) {
         new ct_ignite_number_input_control(
             $wp_customize, 'logo_positioning_leftright_setting',
             array(
-                'label' => 'Left/right',
+                'label' => __('Left/right', 'ignite'),
                 'section' => 'ct-logo-positioning',
                 'settings' => 'logo_positioning_leftright_setting',
                 'type' => 'number',
@@ -112,7 +111,7 @@ function ct_ignite_customize_logo_positioning( $wp_customize ) {
 }
 add_action( 'customize_register', 'ct_ignite_customize_logo_positioning' );
 
-/* need custom function because WP only supplies absint and negative numbers need to be allowed */
+// simply using 'intval' is throwing an error, so using this custom sanitization function
 function ct_ignite_sanitize_integer($input){
     return intval( $input );
 }
@@ -124,7 +123,7 @@ function ct_ignite_customize_logo_size( $wp_customize ) {
     $wp_customize->add_section(
         'ct-logo-size',
         array(
-            'title'      => esc_html__( 'Logo Size', 'ignite' ),
+            'title'      => __( 'Logo Size', 'ignite' ),
             'priority'   => 32,
             'capability' => 'edit_theme_options'
         )
@@ -150,7 +149,7 @@ function ct_ignite_customize_logo_size( $wp_customize ) {
         new ct_ignite_number_input_control(
             $wp_customize, 'logo_size_width_setting',
             array(
-                'label' => 'Increase max-width',
+                'label' => __('Increase max-width', 'ignite'),
                 'section' => 'ct-logo-size',
                 'settings' => 'logo_size_width_setting',
                 'type' => 'number',
@@ -162,7 +161,7 @@ function ct_ignite_customize_logo_size( $wp_customize ) {
         new ct_ignite_number_input_control(
             $wp_customize, 'logo_size_height_setting',
             array(
-                'label' => 'Increase max-height',
+                'label' => __('Increase max-height', 'ignite'),
                 'section' => 'ct-logo-size',
                 'settings' => 'logo_size_height_setting',
                 'type' => 'number',
@@ -201,7 +200,7 @@ function ct_ignite_add_social_sites_customizer($wp_customize) {
         }
     }
 	$wp_customize->add_section( 'ct_ignite_social_settings', array(
-			'title'          => 'Social Media Icons',
+        'title'          => __('Social Media Icons', 'ignite'),
 			'priority'       => 35,
 	) );
 		
@@ -219,7 +218,7 @@ function ct_ignite_add_social_sites_customizer($wp_customize) {
 		$wp_customize->add_control(
             new ct_ignite_url_input_control(
             $wp_customize, $social_site, array(
-				'label'   => __( "$social_site url:", 'ct_ignite_icon' ),
+				'label'   => __( $social_site . " url:", 'ignite' ),
 				'section' => 'ct_ignite_social_settings',
 				'priority'=> $priority,
 		    )
@@ -237,7 +236,7 @@ function ct_ignite_show_author_meta( $wp_customize ) {
     $wp_customize->add_section(
         'ct-author-meta',
         array(
-            'title'      => esc_html__( 'Post Author Info', 'ignite' ),
+            'title'      => __( 'Post Author Info', 'ignite' ),
             'priority'   => 70,
             'capability' => 'edit_theme_options'
         )
@@ -261,8 +260,8 @@ function ct_ignite_show_author_meta( $wp_customize ) {
             'settings'       => 'ct_ignite_author_meta_settings',
             'type'           => 'radio',
             'choices'        => array(
-                'show'   => 'Show',
-                'hide'  => 'Hide'
+                'show'   => __('Show', 'ignite'),
+                'hide'  => __('Hide', 'ignite')
             )
         )
     );
@@ -272,8 +271,8 @@ add_action( 'customize_register', 'ct_ignite_show_author_meta' );
 /* sanitize the radio button input */
 function ct_ignite_sanitize_author_meta_settings($input){
     $valid = array(
-        'show' => 'Show',
-        'hide' => 'Hide'
+        'show'   => __('Show', 'ignite'),
+        'hide'  => __('Hide', 'ignite')
     );
 
     if ( array_key_exists( $input, $valid ) ) {
@@ -290,7 +289,7 @@ function ct_ignite_additional_options( $wp_customize ) {
     $wp_customize->add_section(
         'ct-additional-options',
         array(
-            'title'      => esc_html__( 'Additional Options', 'ignite' ),
+            'title'      => __( 'Additional Options', 'ignite' ),
             'priority'   => 80,
             'capability' => 'edit_theme_options'
         )
@@ -313,8 +312,8 @@ function ct_ignite_additional_options( $wp_customize ) {
             'settings'       => 'ct_ignite_show_full_post_setting',
             'type'           => 'radio',
             'choices'        => array(
-                'yes'   => 'Yes',
-                'no'  => 'No'
+                'yes'   => __('Yes', 'ignite'),
+                'no'  => __('No', 'ignite')
             )
         )
     );
@@ -324,8 +323,8 @@ add_action( 'customize_register', 'ct_ignite_additional_options' );
 /* sanitize the radio button input */
 function ct_ignite_sanitize_show_full_post_setting($input){
     $valid = array(
-        'yes' => 'Yes',
-        'no' => 'No'
+        'yes'   => __('Yes', 'ignite'),
+        'no'  => __('No', 'ignite')
     );
 
     if ( array_key_exists( $input, $valid ) ) {
@@ -337,7 +336,7 @@ function ct_ignite_sanitize_show_full_post_setting($input){
 
 /* create theme options page */
 function ct_ignite_register_theme_page(){
-    add_theme_page( 'Theme Options', 'Theme Options', 'edit_theme_options', 'ignite-options', 'ct_ignite_options_content', 'ct_ignite_options_content');
+    add_theme_page( 'Upgrade to Ignite Plus', 'Upgrade', 'edit_theme_options', 'ignite-options', 'ct_ignite_options_content', 'ct_ignite_options_content');
 }
 add_action( 'admin_menu', 'ct_ignite_register_theme_page' );
 
@@ -345,67 +344,46 @@ add_action( 'admin_menu', 'ct_ignite_register_theme_page' );
 function ct_ignite_options_content(){
     ?>
     <div id="ignite-dashboard-wrap" class="wrap">
-        <h2>Ignite Dashboard</h2>
-        <p>Thanks for downloading Ignite! If you can, please take one minute to <a target="_blank" href="http://wordpress.org/support/view/theme-reviews/ignite"><strong>leave a review</strong></a>.</p>
+        <h2><?php _e('Ignite Dashboard', 'ignite'); ?></h2>
+        <p>
+            <?php
+            $review_url = "http://wordpress.org/support/view/theme-reviews/ignite";
+            printf( __('Thanks for downloading Ignite! If you can, please take one minute to <a target="_blank" href="%s"><strong>leave a review</strong></a>.', 'ignite'), esc_url($review_url));
+            ?>
+        </p>
         <hr />
         <div>
-            <h3>Customization</h3>
-            <p>Ignite now has more customization options! Try out the new logo size & position tools with the customizer.</p>
-            <p><a class="button-primary" href="customize.php">Use the customizer</a></p>
+            <h3><?php _e('Premium Upgrade', 'ignite'); ?></h3>
+            <p><?php _e('Get more flexibility and customizations for your site, with Ignite Plus ($29).', 'ignite'); ?></p>
+            <p><?php _e('Custom colors, custom backgrounds, over 600+ fonts, and more...', 'ignite'); ?></p>
+            <p>
+                <a target="_blank" class="button-primary" href="http://www.competethemes.com/ignite-plus/"><?php _e('View Ignite Plus', 'ignite'); ?></a>
+            </p>
         </div>
         <div class="support">
-            <h3>Support</h3>
+            <h3><?php _e('Support', 'ignite'); ?></h3>
             <ol>
-                <li><a target="_blank" href="http://www.competethemes.com/documentation/ignite-knowledgebase/?utm_source=WordPress%20Dashboard&utm_medium=User%20Admin&utm_content=Ignite&utm_campaign=Admin%20Support%20Widgets">Visit the knowledgebase</a> for self-help.</li>
-                <li><a target="_blank" href="http://wordpress.org/support/theme/ignite">Visit the support forum</a> for community support.</li>
+                <li>
+                    <?php
+                    $docs_url = 'http://www.competethemes.com/documentation/ignite-knowledgebase/?utm_source=WordPress%20Dashboard&utm_medium=User%20Admin&utm_content=Ignite&utm_campaign=Admin%20Support%20Widgets';
+                    $docs_link = sprintf( __( '<a target="_blank" href="%s">Visit the knowledgebase</a> for self-help.', 'ignite' ), esc_url( $docs_url ) );
+                    echo $docs_link;
+                    ?>
+                </li>
+                <li>
+                    <?php
+                    $forum_url = 'http://wordpress.org/support/theme/ignite';
+                    $forum_link = sprintf( __( '<a target="_blank" href="%s">Visit the support forum</a> for community support.', 'ignite' ), esc_url( $forum_url ) );
+                    echo $forum_link;
+                    ?>
+                </li>
             </ol>
-            <p>I (Ben) visit the support forum everyday, so any questions you have will be answered there.</p>
+            <p><?php _e('I (Ben) visit the support forum everyday, so any questions you have will be answered there.', 'ignite'); ?></p>
         </div>
         <div>
-            <h3>Premium</h3>
-            <p>There is now a premium version of Ignite called "Ignite Plus" available.</p>
-            <p><a target="_blank" class="button-primary" href="http://www.competethemes.com/ignite-plus/">View Ignite Plus</a></p>
+            <h3><?php _e('Customize', 'ignite'); ?></h3>
+            <p><?php _e('Customize your site with your logo, social profiles, and more with the built-in Customizer.', 'ignite'); ?></p>
+            <p><a class="button-primary" href="customize.php"><?php _e('Use the customizer', 'ignite'); ?></a></p>
         </div>
-    </div>
-    <hr />
-    <div id="ignite-premium-wrap" class="wrap">
-        <h2>Ignite Plus</h2>
-        <p>Enjoy greater customization and functionality with the Ignite Plus upgrade.</p>
-        <div class="odd">
-            <h2>Custom Background</h2>
-            <p>Change the background of Ignite to any color, any image, or one of 21 bundled textures.</p>
-            <img src="<?php echo get_template_directory_uri() . '/assets/images/custom-background.png'; ?>" />
-        </div>
-        <div class="even">
-            <h2>Custom Colors</h2>
-            <p>Change the colors of Ignite to match your brand or personal preference and watch your site update instantly.</p>
-            <img src="<?php echo get_template_directory_uri() . '/assets/images/ignite-custom-colors.jpg'; ?>" />
-        </div>
-        <div class="odd">
-            <h2>3 New Widgets</h2>
-            <p>Ignite Plus includes a social media icons, recent posts w/ thumbnails, and recent comments w/ thumbnails widgets.</p>
-            <img src="<?php echo get_template_directory_uri() . '/assets/images/ignite-new-widgets.jpg'; ?>" />
-        </div>
-        <div class="even">
-            <h2>Layout Options</h2>
-            <p>Switch between a left-sidebar and right-sidebar layout with a click of a button.</p>
-            <img src="<?php echo get_template_directory_uri() . '/assets/images/ignite-layout.jpg'; ?>" />
-        </div>
-        <div class="odd">
-            <h2>Author-specific Social Icons</h2>
-            <p>Allow every author on your site to include social icons after every post.</p>
-            <img src="<?php echo get_template_directory_uri() . '/assets/images/ignite-author-social-icons.jpg'; ?>" />
-        </div>
-        <div class="even">
-            <h2>After-content Widget Area</h2>
-            <p>Use the after-content widget area to add email signup forms or other widgets after every blog post.</p>
-            <img src="<?php echo get_template_directory_uri() . '/assets/images/ignite-after-content-widget.jpg'; ?>" />
-        </div>
-        <div class="odd">
-            <h2>Footer Menu</h2>
-            <p>The additional footer menu is perfect for your TOS, privacy policy, and other required pages that don't need a lot of visibility.</p>
-            <img src="<?php echo get_template_directory_uri() . '/assets/images/ignite-footer-menu.jpg'; ?>" />
-        </div>
-        <p class="button-wrap"><a target="_blank" class="button-primary" href="http://www.competethemes.com/ignite-plus/">Download Ignite Plus</a></p>
     </div>
     <?php }
