84ae35f0c1e74930d30afada3dcce37348d70b19
BenSibley <ben@competethemes.com>
2014-08-08 14:00:57 -0400

    added profile image upload JS

---
+++ js/profile-uploader.js
@@ -0,0 +1,42 @@
+/*
+ * Adapted from: http://mikejolley.com/2012/12/using-the-new-wordpress-3-5-media-uploader-in-plugins/
+ */
+jQuery(document).ready(function($){
+// Uploading files
+    var file_frame;
+
+    $('#user-profile-upload').on('click', function( event ){
+
+        event.preventDefault();
+
+        // If the media frame already exists, reopen it.
+        if ( file_frame ) {
+            file_frame.open();
+            return;
+        }
+
+        // Create the media frame.
+        file_frame = wp.media.frames.file_frame = wp.media({
+            title: $( this ).data( 'uploader_title' ),
+            button: {
+                text: $( this ).data( 'uploader_button_text' ),
+            },
+            multiple: false  // Set to true to allow multiple files to be selected
+        });
+
+        // When an image is selected, run a callback.
+        file_frame.on( 'select', function() {
+            // We set multiple to false so only get one image from the uploader
+            attachment = file_frame.state().get('selection').first().toJSON();
+
+            // Do something with attachment.id and/or attachment.url here
+            console.log(attachment.url);
+            $('#user_profile_image').val(attachment.url);
+            $('#image-preview').attr('src', attachment.url);
+        });
+
+        // Finally, open the modal
+        file_frame.open();
+    });
+
+});
