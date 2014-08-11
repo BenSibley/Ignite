fb73ce77cc9158b6ac32dbee2b1bc9bcf465531b
BenSibley <ben@competethemes.com>
2014-08-11 14:00:43 -0400

    updated to new function for getting image ID from its URL

--- functions.php
+++ functions.php
@@ -533,8 +533,26 @@ function ct_ignite_show_avatars_check($classes){
 add_action('comment_class', 'ct_ignite_show_avatars_check');
 
 // retrieves the attachment ID from the file URL
-function ct_ignite_get_image_id($image_url) {
+function ct_ignite_get_image_id($url) {
+
+    // Split the $url into two parts with the wp-content directory as the separator
+    $parsed_url  = explode( parse_url( WP_CONTENT_URL, PHP_URL_PATH ), $url );
+
+    // Get the host of the current site and the host of the $url, ignoring www
+    $this_host = str_ireplace( 'www.', '', parse_url( home_url(), PHP_URL_HOST ) );
+    $file_host = str_ireplace( 'www.', '', parse_url( $url, PHP_URL_HOST ) );
+
+    // Return nothing if there aren't any $url parts or if the current host and $url host do not match
+    if ( ! isset( $parsed_url[1] ) || empty( $parsed_url[1] ) || ( $this_host != $file_host ) ) {
+        return;
+    }
+
+    // Now we're going to quickly search the DB for any attachment GUID with a partial path match
+    // Example: /uploads/2013/05/test-image.jpg
     global $wpdb;
-    $attachment = $wpdb->get_col($wpdb->prepare("SELECT ID FROM $wpdb->posts WHERE guid='%s';", $image_url ));
+
+    $attachment = $wpdb->get_col( $wpdb->prepare( "SELECT ID FROM {$wpdb->prefix}posts WHERE guid RLIKE %s;", $parsed_url[1] ) );
+
+    // Returns null if no attachment is found
     return $attachment[0];
 }
