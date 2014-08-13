de4efb709acbd53360947157a3194ca39c576270
BenSibley <ben@competethemes.com>
2014-08-13 12:07:05 -0400

    fixed navigation issues in FF

--- gruntfile.js
+++ gruntfile.js
@@ -85,6 +85,24 @@ module.exports = function(grunt) {
                     type: 'wp-theme'
                 }
             }
+        },
+        phpcs: {
+            application: {
+                dir: ['*.php']
+            },
+            options: {
+                tabWidth: 4
+            }
+        },
+        phpunit: {
+            classes: {
+                dir: 'tests/php/'
+            },
+            options: {
+                bin: 'vendor/bin/phpunit',
+                bootstrap: 'tests/php/phpunit.php',
+                colors: true
+            }
         }
     });
 
@@ -97,8 +115,10 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-compress');
     grunt.loadNpmTasks('grunt-wp-i18n');
+    grunt.loadNpmTasks('grunt-phpcs');
+    grunt.loadNpmTasks('grunt-phpunit');
 
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['concat', 'uglify', 'watch', 'sass', 'autoprefixer', 'cssmin', 'compress', 'makepot']);
+    grunt.registerTask('default', ['concat', 'uglify', 'watch', 'sass', 'autoprefixer', 'cssmin', 'compress', 'makepot', 'phpcs', 'phpunit']);
 
 };
