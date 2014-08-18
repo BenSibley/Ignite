98ef4ca2546e1df8bfebc689d68ec06336e5e93e
BenSibley <ben@competethemes.com>
2014-08-18 12:14:42 -0400

    updated theme options page and removed unneccessary images folder

--- theme-options.php
+++ theme-options.php
@@ -16,45 +16,44 @@ function ct_ignite_options_content(){
 
         <h2 class="nav-tab-wrapper">
             <a href="?page=ignite-options&tab=getting-started" class="nav-tab <?php echo $active_tab == 'getting-started' ? 'nav-tab-active' : ''; ?>">Getting Started</a>
+            <a href="?page=ignite-options&tab=support" class="nav-tab <?php echo $active_tab == 'support' ? 'nav-tab-active' : ''; ?>">Support</a>
             <a href="?page=ignite-options&tab=premium-upgrade" class="nav-tab <?php echo $active_tab == 'premium-upgrade' ? 'nav-tab-active' : ''; ?>">Premium Upgrade</a>
-            <a href="?page=ignite-options&tab=license" class="nav-tab <?php echo $active_tab == 'license' ? 'nav-tab-active' : ''; ?>">License</a>
         </h2>
 
         <?php
         if($active_tab == 'getting-started'){ ?>
             <div class="content content-getting-started">
                 <h3><?php _e('Start Customizing', 'ignite'); ?></h3>
-                <p><?php _e("Ignite's customization options are available in the Customizer. You can click the 'Customize' option in your menu, or use the button below to get started", "ignite") ?>.</p>
+                <p>Thanks for downloading Ignite!</p>
+                <p><?php _e("To start customizing Ignite, click the 'Customize' option in your menu, or use the button below to get started", 'ignite'); ?>.</p>
                 <p>
                     <a class="button-primary" href="customize.php"><?php _e('Use Customizer', 'ignite') ?></a>
                 </p>
+                <p>If you're not sure where to start, you can follow the Getting Started section in the <a target="_blank" href="http://www.competethemes.com/documentation/ignite-knowledgebase/">Ignite Knowledgebase</a>.</p>
+            </div>
+        <?php }
+        elseif($active_tab == 'support'){ ?>
+            <div class="content content-support">
                 <h3><?php _e('Get Support', 'ignite'); ?></h3>
                 <p><?php _e("You can find the knowledgebase, tutorials, support forum, and more in the Ignite Support Center", "ignite"); ?>.</p>
                 <p>
-                    <a target="_blank" class="button-primary" href="http://www.competethemes.com/ignite-support-center/"><?php _e('Visit Support Center', 'ignite'); ?></a>
+                    <a target="_blank" class="button-primary" href="http://www.competethemes.com/documentation/ignite-support-center/"><?php _e('Visit Support Center', 'ignite'); ?></a>
                 </p>
             </div>
         <?php }
         elseif($active_tab == 'premium-upgrade'){ ?>
             <div class="content content-premium-upgrade">
-                <h3><?php _e('Ignite Plus ($29)', 'ignite'); ?></h3>
-                <p><?php _e('Upgrade to Ignite Plus and get:', 'ignite'); ?></p>
-                <ul style="list-style: disc; margin-left: 36px">
+                <h3><?php _e('Upgrade to Ignite Plus ($29)', 'ignite'); ?></h3>
+                <p><?php _e('Ignite Plus is the premium version of Ignite. By upgrading to Ignite Plus, you get:', 'ignite'); ?></p>
+                <ul>
                     <li><?php _e('Custom colors', 'ignite'); ?></li>
                     <li><?php _e('Background images & textures', 'ignite'); ?></li>
-                    <li><?php _e('Six new layouts', 'ignite'); ?></li>
+                    <li><?php _e('New layouts', 'ignite'); ?></li>
                     <li><?php _e('and much more&#8230;', 'ignite'); ?></li>
                 </ul>
                 <p>
-                    <a target="_blank" class="button-primary" href="https://www.competethemes.com/ignite-plus/"><?php _e('See Ignite Plus', 'ignite'); ?></a>
+                    <a target="_blank" class="button-primary" href="https://www.competethemes.com/ignite-plus/"><?php _e('See Full Feature List', 'ignite'); ?></a>
                 </p>
-            </div>
-        <?php }
-        elseif($active_tab == 'license'){ ?>
-            <div class="content content-license">
-                <h3><?php _e('Customize', 'ignite'); ?></h3>
-                <p><?php _e('Customize your site with your logo, social profiles, and more with the built-in Customizer.', 'ignite'); ?></p>
-                <p><a class="button-primary" href="customize.php"><?php _e('Use the customizer', 'ignite'); ?></a></p>
             </div>
         <?php } ?>
     </div>
