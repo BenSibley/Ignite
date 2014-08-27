eb942a890a393949a9189dbb3c45fd0ea5919f9b
BenSibley <ben@competethemes.com>
2014-08-27 10:43:44 -0400

    removed home in favor of index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 if ( have_posts() ) :
     while (have_posts() ) : 
         the_post();
-        get_template_part('content', get_post_format() );
+        get_template_part('content');
     endwhile;
 endif; ?>
 
