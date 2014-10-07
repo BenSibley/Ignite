11ff6a37bff16f8adc264fb14ade9b6c3345a917
BenSibley <ben@competethemes.com>
2014-10-07 14:23:50 -0400

    added image widget

---
+++ inc/widgets/image_widget.php
@@ -0,0 +1,131 @@
+<?php
+/**
+ * Adds Recent Comments widget.
+ */
+class ct_ignite_Image extends WP_Widget {
+
+	/**
+	 * Register widget with WordPress.
+	 */
+	function __construct() {
+
+		/* Set up the widget options. */
+		$widget_options = array(
+			'classname'   => 'widget_ct_ignite_image', // Classes
+			'description' => __( 'This widget displays an image', 'ignite' ) // Args
+		);
+
+		/* Create the widget. */
+		$this->WP_Widget(
+			'ct_ignite_image', // Base ID
+			__('Image','ignite'), // Name
+			$widget_options
+		);
+	}
+
+	/**
+	 * Front-end display of widget.
+	 *
+	 * @see WP_Widget::widget()
+	 *
+	 * @param array $args     Widget arguments.
+	 * @param array $instance Saved values from database.
+	 */
+	public function widget( $args, $instance ) {
+
+		/* the sidebar's $before_widget wrapper. */
+		$html =  $args['before_widget'];
+
+		/* If an image was uploaded by the user, display it. */
+		if ($instance['image']) {
+			$html .= "<img title='" . $instance['title'] . "' alt='" . $instance['alt-text'] . "' src='". $instance['image'] ."' />";
+		}
+
+		/* close the widget </section> */
+		$html .=  $args['after_widget'];
+
+		echo $html;
+	}
+
+	/**
+	 * Back-end widget form.
+	 *
+	 * @see WP_Widget::form()
+	 *
+	 * @param array $instance Previously saved values from database.
+	 */
+	public function form( $instance ) {
+
+		/* Image */
+		if ( isset( $instance[ 'image' ] ) ) {
+			$image = $instance[ 'image' ];
+		} else {
+			$image = __( 'http://', 'ignite' );
+		}
+
+		/* Title */
+		if ( isset( $instance[ 'title' ] ) ) {
+			$title = $instance[ 'title' ];
+		} else {
+			$title = __( '', 'ignite' );
+		}
+
+		/* Alt text */
+		if ( isset( $instance[ 'alt-text' ] ) ) {
+			$alt_text = $instance[ 'alt-text' ];
+		} else {
+			$alt_text = __( '', 'ignite' );
+		}
+
+		?>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'image' ); ?>"><?php _e( 'Image URL:','ignite' ); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id( 'image' ); ?>" name="<?php echo $this->get_field_name( 'image' ); ?>" type="text" value="<?php echo esc_url( $image ); ?>">
+			<input type='button' id="image-upload" class="button-primary" value="<?php _e( 'Upload Image', 'ignite' ); ?>"/>
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:','ignite' ); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'alt-text' ); ?>"><?php _e( 'Alternate text','ignite' ); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id( 'alt-text' ); ?>" name="<?php echo $this->get_field_name( 'alt-text' ); ?>" type="text" value="<?php echo esc_attr( $alt_text ); ?>">
+		</p>
+
+
+	<?php
+	}
+
+	/**
+	 * Sanitize widget form values as they are saved.
+	 *
+	 * @see WP_Widget::update()
+	 *
+	 * @param array $new_instance Values just sent to be saved.
+	 * @param array $old_instance Previously saved values from database.
+	 *
+	 * @return array Updated safe values to be saved.
+	 */
+	public function update( $new_instance, $old_instance ) {
+
+		$instance = array();
+
+		/* image */
+		$instance['image'] = ( ! empty( $new_instance['image'] ) ) ? strip_tags( $new_instance['image'] ) : '';
+
+		/* title */
+		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+
+		/* alt text */
+		$instance['alt-text'] = ( ! empty( $new_instance['alt-text'] ) ) ? strip_tags( $new_instance['alt-text'] ) : '';
+
+		return $instance;
+	}
+
+}
+
+// register widget
+function register_ct_ignite_image_widget() {
+	register_widget( 'ct_ignite_image' );
+}
+add_action( 'widgets_init', 'register_ct_ignite_image_widget' );
