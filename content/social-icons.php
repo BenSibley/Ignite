25adc4988b1aac7eed7eb796a83420432cb48587
BenSibley <ben@competethemes.com>
2014-10-14 10:05:22 -0400

    added escaping to social icon classes

--- content/social-icons.php
+++ content/social-icons.php
@@ -23,13 +23,13 @@ if(!empty($active_sites)) {
 	<?php endif; ?>
 
 		<?php if( $active_site ==  "flickr" || $active_site ==  "dribbble" || $active_site ==  "instagram" || $active_site ==  "soundcloud" || $active_site ==  "spotify" || $active_site ==  "vine" || $active_site ==  "yahoo" || $active_site ==  "codepen" || $active_site ==  "delicious" || $active_site ==  "stumbleupon" || $active_site ==  "deviantart" || $active_site ==  "digg" || $active_site ==  "hacker-news" || $active_site == "vk" || $active_site == 'weibo' || $active_site == 'tencent-weibo') { ?>
-			<i class="fa fa-<?php echo $active_site; ?>"></i>
+			<i class="fa fa-<?php echo esc_attr($active_site); ?>"></i>
 		<?php } elseif( $active_site == 'email' ) { ?>
 			<i class="fa fa-envelope"></i>
 		<?php } elseif( $active_site == 'academia' ) { ?>
 			<i class="fa fa-graduation-cap"></i>
 		<?php } else { ?>
-			<i class="fa fa-<?php echo $active_site; ?>-square"></i>
+			<i class="fa fa-<?php echo esc_attr($active_site); ?>-square"></i>
 		<?php } ?>
 		</a>
 		</li><?php
