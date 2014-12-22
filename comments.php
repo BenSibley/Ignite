aaa6a5213511271c8883443d16a5acac349ef023
BenSibley <ben@competethemes.com>
2014-12-22 16:09:43 -0500

    fixed formatting and extra </section> in comments.php

--- comments.php
+++ comments.php
@@ -11,56 +11,59 @@ if ( comments_open() ) { ?>
                 <?php comments_number( __( 'Be First to Comment', 'ignite' ), __( 'One Comment', 'ignite'), __( '% Comments', 'ignite' ) ); ?>
             </h3>
         </div>
-            <ol class="comment-list">
-                    <?php wp_list_comments(array( 'callback'=>'ct_ignite_customize_comments', 'max_depth'=>'7') ); ?>
-            </ol><!-- .comment-list -->
-            <?php    
-            if( (get_option('page_comments') == 1) && (get_comment_pages_count() > 1) ) { ?>
-                <nav class="comment-pagination">
-                    <p class="previous-comment"><?php previous_comments_link(); ?></p><p class="next-comment"><?php next_comments_link(); ?></p>
-                </nav>
-            <?php } ?>
-            <?php comment_form(); ?>
+        <ol class="comment-list">
+                <?php wp_list_comments(array( 'callback'=>'ct_ignite_customize_comments', 'max_depth'=>'7') ); ?>
+        </ol><!-- .comment-list -->
+        <?php
+        if( (get_option('page_comments') == 1) && (get_comment_pages_count() > 1) ) { ?>
+            <nav class="comment-pagination">
+                <p class="previous-comment"><?php previous_comments_link(); ?></p><p class="next-comment"><?php next_comments_link(); ?></p>
+            </nav>
+        <?php } ?>
+        <?php comment_form(); ?>
+    </section><!-- #comments -->
 <?php
 } elseif(!comments_open() && have_comments() && pings_open() ) { ?>
     <section id="comments" class="comments">
         <div class="comments-number">
             <h3>
-<?php comments_number( __( 'Be First to Comment', 'ignite' ), __( 'One Comment', 'ignite'), __( '% Comments', 'ignite' ) ); ?>
+                <?php comments_number( __( 'Be First to Comment', 'ignite' ), __( 'One Comment', 'ignite'), __( '% Comments', 'ignite' ) ); ?>
             </h3>
         </div>
-            <ol class="comment-list">
-                    <?php wp_list_comments(array( 'callback'=>'ct_ignite_customize_comments', 'max_depth'=>'3') ); ?>
-            </ol><!-- .comment-list -->
-            <?php    
-            if( (get_option('page_comments') == 1) && (get_comment_pages_count() > 1) ) { ?>
-                <nav class="comment-pagination">
-                    <p class="previous-comment"><?php previous_comments_link(); ?></p><p class="next-comment"><?php next_comments_link(); ?></p>
-                </nav>
-            <?php } ?>
-            <p class="comments-closed pings-open">
-                    <?php printf( __( 'Comments are closed, but <a href="%s" title="Trackback URL for this post">trackbacks</a> and pingbacks are open.', 'ignite' ), esc_url( get_trackback_url() ) ); ?>
-            </p><!-- .comments-closed .pings-open -->
+        <ol class="comment-list">
+                <?php wp_list_comments(array( 'callback'=>'ct_ignite_customize_comments', 'max_depth'=>'3') ); ?>
+        </ol><!-- .comment-list -->
+        <?php
+        if( (get_option('page_comments') == 1) && (get_comment_pages_count() > 1) ) { ?>
+            <nav class="comment-pagination">
+                <p class="previous-comment"><?php previous_comments_link(); ?></p><p class="next-comment"><?php next_comments_link(); ?></p>
+            </nav>
+        <?php } ?>
+        <p class="comments-closed pings-open">
+                <?php printf( __( 'Comments are closed, but <a href="%s" title="Trackback URL for this post">trackbacks</a> and pingbacks are open.', 'ignite' ), esc_url( get_trackback_url() ) ); ?>
+        </p><!-- .comments-closed .pings-open -->
+    </section><!-- #comments -->
 <?php
 } elseif ( !comments_open() && have_comments() ) { ?>
     <section id="comments" class="comments">
         <div class="comments-number">
             <h3>
-<?php comments_number( __( 'Be First to Comment', 'ignite' ), __( 'One Comment', 'ignite'), __( '% Comments', 'ignite' ) ); ?>
+                <?php comments_number( __( 'Be First to Comment', 'ignite' ), __( 'One Comment', 'ignite'), __( '% Comments', 'ignite' ) ); ?>
             </h3>
         </div>
-            <ol class="comment-list">
-                    <?php wp_list_comments(array( 'callback'=>'ct_ignite_customize_comments', 'max_depth'=>'3') ); ?>
-            </ol><!-- .comment-list -->
-            <?php    
-            if( (get_option('page_comments') == 1) && (get_comment_pages_count() > 1) ) { ?>
-                <nav class="comment-pagination">
-                    <p class="previous-comment"><?php previous_comments_link(); ?></p><p class="next-comment"><?php next_comments_link(); ?></p>
-                </nav>
-            <?php } ?>
-            <p class="comments-closed">
-                <?php _e( 'Comments are closed.', 'ignite' ); ?>
-            </p><!-- .comments-closed -->
+        <ol class="comment-list">
+                <?php wp_list_comments(array( 'callback'=>'ct_ignite_customize_comments', 'max_depth'=>'3') ); ?>
+        </ol><!-- .comment-list -->
+        <?php
+        if( (get_option('page_comments') == 1) && (get_comment_pages_count() > 1) ) { ?>
+            <nav class="comment-pagination">
+                <p class="previous-comment"><?php previous_comments_link(); ?></p><p class="next-comment"><?php next_comments_link(); ?></p>
+            </nav>
+        <?php } ?>
+        <p class="comments-closed">
+            <?php _e( 'Comments are closed.', 'ignite' ); ?>
+        </p><!-- .comments-closed -->
+    </section><!-- #comments -->
 <?php 
 } else { ?>
 
@@ -69,5 +72,3 @@ if ( comments_open() ) { ?>
     </p><!-- .comments-closed -->
 
 <?php } ?>
-  
-    </section><!-- #comments -->
