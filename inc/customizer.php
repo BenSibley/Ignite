0e22566aa75e9a17ce88420a84074e0e51352296
BenSibley <ben@competethemes.com>
2015-03-23 10:33:14 -0400

    updated custom css filter

--- inc/customizer.php
+++ inc/customizer.php
@@ -545,7 +545,7 @@ function ct_ignite_add_customizer_content( $wp_customize ) {
     $wp_customize->add_setting( 'ct_ignite_custom_css_setting', array(
         'type'              => 'theme_mod',
         'capability'        => 'edit_theme_options',
-        'sanitize_callback' => 'esc_textarea',
+        'sanitize_callback' => 'wp_filter_nohtml_kses',
     ) );
     // control
     $wp_customize->add_control( new ct_ignite_Textarea_Control(
