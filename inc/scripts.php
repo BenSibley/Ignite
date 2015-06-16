809f19d72308f5027fad9f7d7c58fe770c243ecb
BenSibley <ben@competethemes.com>
2015-06-16 14:25:04 -0400

    now using get_stylesheet_uri for better child theme compatibility

--- inc/scripts.php
+++ inc/scripts.php
@@ -20,7 +20,7 @@ function ct_ignite_load_scripts_styles() {
 		wp_enqueue_style('font-awesome', get_template_directory_uri() . '/assets/font-awesome/css/font-awesome.min.css');
 
 		// Stylesheet
-		wp_enqueue_style('style', get_template_directory_uri() . 'style.min.css');
+		wp_enqueue_style('style', get_stylesheet_uri() );
 	}
 	// enqueue comment-reply script only on posts & pages with comments open ( included in WP core )
 	if( is_singular() && comments_open() && get_option('thread_comments') ) {
