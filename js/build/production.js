98c38418aa87586dacddb464ccb9939b5a8fe73d
BenSibley <ben@competethemes.com>
2015-06-17 11:10:08 -0400

    setup grunt and built minified postMessage file

--- js/build/production.js
+++ js/build/production.js
@@ -772,3 +772,30 @@ window.addEventListener("hashchange", function(event) {
     Placeholders.enable = Placeholders.nativeSupport ? noop : enablePlaceholders;
 
 }(this));
+( function( $ ) {
+
+    /*
+     * Following functions are for utilizing the postMessage transport setting
+     */
+
+    // Site title
+    wp.customize( 'blogname', function( value ) {
+        value.bind( function( to ) {
+            // if there is a logo, don't replace it
+            if( $('.site-title').find('img').length == 0 ) {
+                $( '.site-title a' ).text( to );
+            }
+        } );
+    } );
+    // Tagline
+    wp.customize( 'blogdescription', function( value ) {
+        value.bind( function( to ) {
+            var tagline = $('#site-description');
+            if( tagline.length == 0 ) {
+                $('#title-container').append('<p id="site-description"></p>');
+            }
+            tagline.text( to );
+        } );
+    } );
+
+} )( jQuery );
