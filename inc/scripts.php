c2a7a4f4400e2113587c7b5ff498eae61577b3a1
BenSibley <ben@competethemes.com>
2015-08-06 10:40:02 -0400

    removed menubar ARIA role and updated Font Awesome to v4.4.0

--- inc/scripts.php
+++ inc/scripts.php
@@ -7,21 +7,18 @@ function ct_ignite_load_scripts_styles() {
 
 	wp_register_style( 'ct-ignite-google-fonts', '//fonts.googleapis.com/css?family=Lusitana:400,700');
 
-	// enqueue on front-end only
-	if(! is_admin() ) {
+	// main JS file
+	wp_enqueue_script('ct-ignite-production', get_template_directory_uri() . '/js/build/production.min.js#ct_ignite_asyncload', array('jquery'),'', true);
 
-		// main JS file
-		wp_enqueue_script('ct-ignite-production', get_template_directory_uri() . '/js/build/production.min.js#ct_ignite_asyncload', array('jquery'),'', true);
+	// Google Fonts
+	wp_enqueue_style('ct-ignite-google-fonts');
 
-		// Google Fonts
-		wp_enqueue_style('ct-ignite-google-fonts');
+	// Font Awesome
+	wp_enqueue_style('font-awesome', get_template_directory_uri() . '/assets/font-awesome/css/font-awesome.min.css');
 
-		// Font Awesome
-		wp_enqueue_style('font-awesome', get_template_directory_uri() . '/assets/font-awesome/css/font-awesome.min.css');
+	// Stylesheet
+	wp_enqueue_style('style', get_stylesheet_uri() );
 
-		// Stylesheet
-		wp_enqueue_style('style', get_stylesheet_uri() );
-	}
 	// enqueue comment-reply script only on posts & pages with comments open ( included in WP core )
 	if( is_singular() && comments_open() && get_option('thread_comments') ) {
 		wp_enqueue_script( 'comment-reply' );
