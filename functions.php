c37ff4600a97f0df50fb146245381a75cf9cbc99
BenSibley <ben@competethemes.com>
2015-09-02 07:54:14 -0400

    added sanitization to custom css output

--- functions.php
+++ functions.php
@@ -431,6 +431,7 @@ function ct_ignite_custom_css_output(){
 
     /* output custom css */
     if($custom_css) {
+        $custom_css = wp_filter_nohtml_kses( $custom_css );
         wp_add_inline_style('style', $custom_css);
     }
 }
