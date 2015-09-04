11efc7b125406a9783da0f0d51432ae6386fc5e4
BenSibley <ben@competethemes.com>
2015-09-04 07:43:03 -0400

    finished breadcrumbs function

--- inc/breadcrumbs.php
+++ inc/breadcrumbs.php
@@ -4,7 +4,9 @@
 function ct_ignite_breadcrumbs() {
 
 	// Do not display on the homepage
-	if ( is_front_page() ) return;
+	if ( is_front_page() ) {
+		return;
+	}
 
 	// Settings
 	$separator           = '&gt;';
@@ -42,14 +44,13 @@ function ct_ignite_breadcrumbs() {
 
 		// Loop through parent categories and add to breadcrumb trail
 		foreach ( $cat_parents as $parent ) {
-			$html .= '<span class="item-cat">' . wp_kses( $parent, wp_kses_allowed_html('a') ). '</span>';
+			$html .= '<span class="item-cat">' . wp_kses( $parent, wp_kses_allowed_html( 'a' ) ) . '</span>';
 			$html .= '<span class="separator"> ' . esc_attr( $separator ) . ' </span>';
 		}
 
 		// add name of Post
 		$html .= '<span class="item-current item-' . $post->ID . '"><span class="bread-current bread-' . $post->ID . '" title="' . get_the_title() . '">' . get_the_title() . '</span></span>';
-	}
-	// Page
+	} // Page
 	elseif ( is_singular( 'page' ) ) {
 
 		// if page has a parent page
@@ -69,8 +70,7 @@ function ct_ignite_breadcrumbs() {
 		}
 		// Current page
 		$html .= '<span class="item-current item-' . $post->ID . '"><span title="' . get_the_title() . '"> ' . get_the_title() . '</span></span>';
-	}
-	// Attachment
+	} // Attachment
 	elseif ( is_singular( 'attachment' ) ) {
 
 		// Get the parent post ID
@@ -88,18 +88,17 @@ function ct_ignite_breadcrumbs() {
 
 		// Add name of attachment
 		$html .= '<span class="item-current item-' . $post->ID . '"><span title="' . get_the_title() . '"> ' . get_the_title() . '</span></span>';
-	}
-	// Custom Post Types
+	} // Custom Post Types
 	elseif ( is_singular() ) {
 
 		// Get the post type
 		$post_type = get_post_type();
 
 		// Get the post object
-		$post_type_object = get_post_type_object($post_type);
+		$post_type_object = get_post_type_object( $post_type );
 
 		// Get the post type archive
-		$post_type_archive = get_post_type_archive_link($post_type);
+		$post_type_archive = get_post_type_archive_link( $post_type );
 
 		// Add taxonomy link and separator
 		$html .= '<span class="item-cat item-custom-post-type-' . esc_attr( $post_type ) . '"><a class="bread-cat bread-custom-post-type-' . esc_attr( $post_type ) . '" href="' . esc_url( $post_type_archive ) . '" title="' . esc_attr( $post_type_object->labels->name ) . '">' . esc_attr( $post_type_object->labels->name ) . '</a></span>';
@@ -107,215 +106,55 @@ function ct_ignite_breadcrumbs() {
 
 		// Add name of Post
 		$html .= '<span class="item-current item-' . $post->ID . '"><span class="bread-current bread-' . $post->ID . '" title="' . $post->post_title . '">' . $post->post_title . '</span></span>';
-	}
-	// Category
+	} // Category
 	elseif ( is_category() ) {
 
 		// Add category markup
-		$html .= '<span class="item-current item-cat"><span class="bread-current bread-cat" title="' . $post->ID . '">' . single_cat_title('', false) . '</span></span>';
-	}
-	// Tag
+		$html .= '<span class="item-current item-cat"><span class="bread-current bread-cat" title="' . $post->ID . '">' . single_cat_title( '', false ) . '</span></span>';
+	} // Tag
 	elseif ( is_tag() ) {
 
 		// Add tag markup
-		$html .= '<span class="item-current item-tag"><span class="bread-current bread-tag">' . single_tag_title('', false) . '</span></span>';
-	}
-	// Author
+		$html .= '<span class="item-current item-tag"><span class="bread-current bread-tag">' . single_tag_title( '', false ) . '</span></span>';
+	} // Author
 	elseif ( is_author() ) {
 
 		// Add author markup
 		$html .= '<span class="item-current item-author"><span class="bread-current bread-author">' . get_queried_object()->display_name . '</span></span>';
-	}
-	// Day
+	} // Day
 	elseif ( is_day() ) {
 
 		// Add day markup
 		$html .= '<span class="item-current item-day"><span class="bread-current bread-day">' . get_the_date() . '</span></span>';
-	}
-	// Month
+	} // Month
 	elseif ( is_month() ) {
 
 		// Add month markup
-		$html .= '<span class="item-current item-month"><span class="bread-current bread-month">' . get_the_date('F Y') . '</span></span>';
-	}
-	// Year
+		$html .= '<span class="item-current item-month"><span class="bread-current bread-month">' . get_the_date( 'F Y' ) . '</span></span>';
+	} // Year
 	elseif ( is_year() ) {
 
 		// Add year markup
-		$html .= '<span class="item-current item-year"><span class="bread-current bread-year">' . get_the_date('Y') . '</span></span>';
-	}
-	// Search
+		$html .= '<span class="item-current item-year"><span class="bread-current bread-year">' . get_the_date( 'Y' ) . '</span></span>';
+	} // Custom Taxonomy
+	elseif ( is_archive() ) {
+
+		// get the name of the taxonomy
+		$custom_tax_name = get_queried_object()->name;
+
+		// Add markup for taxonomy
+		$html .= '<span class="item-current item-archive"><span class="bread-current bread-archive">' . $custom_tax_name . '</span></span>';
+	} // Search
 	elseif ( is_search() ) {
 
-	}
-	// 404
+		// Add search markup
+		$html .= '<span class="item-current item-search"><span class="bread-current bread-search">Search results for: ' . get_search_query() . '</span></span>';
+	} // 404
 	elseif ( is_404() ) {
 
+		// Add 404 markup
+		$html .= '<span>' . 'Error 404' . '</span>';
 	}
-	// Custom Taxonomy
-	elseif ( is_tax() ) {
-
-	}
-//	if ( is_archive() && !is_tax() && !is_category() ) {
-//
-//		echo '<span class="item-current item-archive"><span class="bread-current bread-archive">' . post_type_archive_title(false, false) . '</span></span>';
-//
-//	} elseif ( is_archive() && is_tax() && !is_category() ) {
-//
-//		$custom_tax_name = get_queried_object()->name;
-//		echo '<span class="item-current item-archive"><span class="bread-current bread-archive">' . $custom_tax_name . '</span></span>';
-//
-//	} elseif ( is_singular( 'post' ) ) {
-//
-//		// Get post category info
-//		$category = get_the_category();
-//
-//		$category_values = array_values($category);
-//
-//		// Get last category post is in
-//		$last_category = end( $category_values );
-//
-//		// Get parent any categories and create array
-//		$get_cat_parents = rtrim(get_category_parents($last_category->term_id, true, ','),',');
-//		$cat_parents = explode(',',$get_cat_parents);
-//
-//		// Loop through parent categories and store in variable $cat_display
-//		$cat_display = '';
-//
-//		foreach($cat_parents as $parents) {
-//			$cat_display .= '<span class="item-cat">'.$parents.'</span>';
-//			$cat_display .= '<span class="separator"> ' . $separator . ' </span>';
-//		}
-//
-//		// Check if the post is in a category
-//		if( !empty( $last_category ) ) {
-//			echo $cat_display;
-//			echo '<span class="item-current item-' . $post->ID . '"><span class="bread-current bread-' . $post->ID . '" title="' . get_the_title() . '">' . get_the_title() . '</span></span>';
-//		} else {
-//			echo '<span class="item-current item-' . $post->ID . '"><span class="bread-current bread-' . $post->ID . '" title="' . get_the_title() . '">' . get_the_title() . '</span></span>';
-//		}
-
-//	} elseif ( is_singular( 'page' ) ) {
-//
-//		// Standard page
-//		if( $post->post_parent ){
-//
-//			// If child page, get parents
-//			$anc = get_post_ancestors( $post->ID );
-//
-//			// Get parents in the right order
-//			$anc = array_reverse($anc);
-//
-//			$parents = '';
-//
-//			// Parent page loop
-//			foreach ( $anc as $ancestor ) {
-//				$parents .= '<span class="item-parent item-parent-' . $ancestor . '"><a class="bread-parent bread-parent-' . $ancestor . '" href="' . get_permalink($ancestor) . '" title="' . get_the_title($ancestor) . '">' . get_the_title($ancestor) . '</a></span>';
-//				$parents .= '<span class="separator separator-' . $ancestor . '"> ' . $separator . ' </span>';
-//			}
-//
-//			// Display parent pages
-//			echo $parents;
-//
-//			// Current page
-//			echo '<span class="item-current item-' . $post->ID . '"><span title="' . get_the_title() . '"> ' . get_the_title() . '</span></span>';
-
-//		} else {
-//
-//			// Just display current page if not parents
-//			echo '<span class="item-current item-' . $post->ID . '"><span class="bread-current bread-' . $post->ID . '"> ' . get_the_title() . '</span></span>';
-//
-//		}
-//
-//	} elseif ( is_singular( 'attachment' ) ) {
-//
-//		// Get the parent post ID
-//		$parent_id = $post->post_parent;
-//
-//		// Get the parent post Title
-//		$parent_title = get_the_title( $parent_id );
-//
-//		// Get the parent post permalink
-//		$parent_permalink = get_permalink( $parent_id );
-//
-//		$parent_markup = '<span class="item-parent"><a class="bread-parent" href="' . $parent_permalink . '" title="' . $parent_title . '">' . $parent_title . '</a></span>';
-//		$parent_markup .= '<span class="separator"> ' . $separator . ' </span>';
-//
-//		echo $parent_markup;
-//
-//		// Current page
-//		echo '<span class="item-current item-' . $post->ID . '"><span title="' . get_the_title() . '"> ' . get_the_title() . '</span></span>';
-//
-//	} elseif ( is_tag() ) {
-//
-//		// Tag page
-//
-//		// Get tag information
-//		$term_id = get_query_var('tag_id');
-//		$taxonomy = 'post_tag';
-//		$args ='include=' . $term_id;
-//		$terms = get_terms( $taxonomy, $args );
-//
-//		// Display the tag name
-//		echo '<span class="item-current item-tag-' . $terms->term_id . ' item-tag-' . $terms[0]->slug . '"><span class="bread-current bread-tag-' . $terms->term_id . ' bread-tag-' . $terms[0]->slug . '">' . $terms[0]->name . '</span></span>';
-//
-//    } elseif ( is_day() ) {
-//
-//		// Day archive
-//
-//		// Year link
-//		echo '<span class="item-year item-year-' . get_the_time('Y') . '"><a class="bread-year bread-year-' . get_the_time('Y') . '" href="' . get_year_link( get_the_time('Y') ) . '" title="' . get_the_time('Y') . '">' . get_the_time('Y') . ' Archives</a></span>';
-//		echo '<span class="separator separator-' . get_the_time('Y') . '"> ' . $separator . ' </span>';
-//
-//		// Month link
-//		echo '<span class="item-month item-month-' . get_the_time('m') . '"><a class="bread-month bread-month-' . get_the_time('m') . '" href="' . get_month_link( get_the_time('Y'), get_the_time('m') ) . '" title="' . get_the_time('M') . '">' . get_the_time('M') . ' Archives</a></span>';
-//		echo '<span class="separator separator-' . get_the_time('m') . '"> ' . $separator . ' </span>';
-//
-//		// Day display
-//		echo '<span class="item-current item-' . get_the_time('j') . '"><span class="bread-current bread-' . get_the_time('j') . '"> ' . get_the_time('jS') . ' ' . get_the_time('M') . ' Archives</span></span>';
-//
-//	} elseif ( is_month() ) {
-//
-//		// Month Archive
-//
-//		// Year link
-//		echo '<span class="item-year item-year-' . get_the_time('Y') . '"><a class="bread-year bread-year-' . get_the_time('Y') . '" href="' . get_year_link( get_the_time('Y') ) . '" title="' . get_the_time('Y') . '">' . get_the_time('Y') . ' Archives</a></span>';
-//		echo '<span class="separator separator-' . get_the_time('Y') . '"> ' . $separator . ' </span>';
-//
-//		// Month display
-//		echo '<span class="item-month item-month-' . get_the_time('m') . '"><span class="bread-month bread-month-' . get_the_time('m') . '" title="' . get_the_time('M') . '">' . get_the_time('M') . ' Archives</span></span>';
-//
-//	} elseif ( is_year() ) {
-//
-//		// Display year archive
-//		echo '<span class="item-current item-current-' . get_the_time('Y') . '"><span class="bread-current bread-current-' . get_the_time('Y') . '" title="' . get_the_time('Y') . '">' . get_the_time('Y') . ' Archives</span></span>';
-//
-//	} elseif ( is_author() ) {
-//
-//		// Auhor archive
-//
-//		// Get the author information
-//		global $author;
-//		$userdata = get_userdata( $author );
-//
-//		// Display author name
-//		echo '<span class="item-current item-current-' . $userdata->user_nicename . '"><span class="bread-current bread-current-' . $userdata->user_nicename . '" title="' . $userdata->display_name . '">' . 'Author: ' . $userdata->display_name . '</span></span>';
-//
-//	} elseif ( get_query_var('paged') ) {
-//
-//		// Paginated archives
-//		echo '<span class="item-current item-current-' . get_query_var('paged') . '"><span class="bread-current bread-current-' . get_query_var('paged') . '" title="Page ' . get_query_var('paged') . '">'.__('Page') . ' ' . get_query_var('paged') . '</span></span>';
-//
-//	} elseif ( is_search() ) {
-//
-//		// Search results page
-//		echo '<span class="item-current item-current-' . get_search_query() . '"><span class="bread-current bread-current-' . get_search_query() . '" title="Search results for: ' . get_search_query() . '">Search results for: ' . get_search_query() . '</span></span>';
-//
-//	} elseif ( is_404() ) {
-//
-//		// 404 page
-//		echo '<span>' . 'Error 404' . '</span>';
-//	}
 
 	// Close breadcrumb container
 	$html .= '</div>';
