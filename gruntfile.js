f0273689c2db849c8e0a227bebce3b3cc4616d14
BenSibley <ben@competethemes.com>
2015-10-06 07:51:09 -0400

    removed 404 styles and updated SASS and grunt

--- gruntfile.js
+++ gruntfile.js
@@ -42,7 +42,8 @@ module.exports = function(grunt) {
         sass: {
             dist: {
                 options: {
-                    style: 'expanded'
+                    style: 'expanded',
+                    sourcemap: 'none'
                 },
                 files: {
                     'style.css': 'sass/style.scss',
