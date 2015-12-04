2942108cf0cb98439665e7c60fbc146d626b36ef
BenSibley <ben@competethemes.com>
2015-12-04 15:25:35 -0500

    featured image sizes across browsers now more reliable

--- js/functions.js
+++ js/functions.js
@@ -189,32 +189,33 @@ jQuery(document).ready(function($){
 
                 var image = $(this).children('img').add( $(this).children('a').children('img') );
 
+                // don't process images twice (relevant when using infinite scroll)
+                if (image.hasClass('no-object-fit')) return;
+
                 image.addClass('no-object-fit');
 
-                // if the image is not tall enough to fill the space
-                if ( image.outerHeight() < $(this).outerHeight()) {
-
-                    // is it also not wide enough?
-                    if ( image.outerWidth() < $(this).outerWidth()) {
-                        image.css({
-                            'min-width': '100%',
-                            'min-height': '100%',
-                            'max-width': 'none',
-                            'max-height': 'none'
-                        });
-                    } else {
-                        image.css({
-                            'height': '100%',
-                            'max-width': 'none'
-                        });
-                    }
-                }
                 // if the image is not wide enough to fill the space
-                else if ( image.outerWidth() < $(this).outerWidth()) {
+                if (image.outerWidth() < $(this).outerWidth()) {
 
                     image.css({
                         'width': '100%',
+                        'min-width': '100%',
+                        'max-width': '100%',
+                        'height': 'auto',
+                        'min-height': '100%',
                         'max-height': 'none'
+                    });
+                }
+                // if the image is not tall enough to fill the space
+                if (image.outerHeight() < $(this).outerHeight()) {
+
+                    image.css({
+                        'height': '100%',
+                        'min-height': '100%',
+                        'max-height': '100%',
+                        'width': 'auto',
+                        'min-width': '100%',
+                        'max-width': 'none'
                     });
                 }
             });
