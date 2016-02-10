ffc37910a30097580e8b44462d092778ea0a67c6
BenSibley <ben@competethemes.com>
2016-02-10 09:22:05 -0500

    made content loop overridable

--- functions.php
+++ functions.php
@@ -543,46 +543,48 @@ add_action( 'wp_head', 'ct_ignite_add_meta_elements', 1 );
 remove_action( 'wp_head', 'wp_generator' );
 add_action( 'wp_head', 'wp_generator', 1 );
 
-function ct_ignite_get_content_template() {
+if ( ! function_exists( 'ct_ignite_get_content_template' ) ) {
+	function ct_ignite_get_content_template() {
 
-	// Blog
-	if ( is_home() ) {
-		get_template_part( 'content' );
-	} // Post
-	elseif ( is_singular( 'post' ) ) {
-		get_template_part( 'content' );
-		comments_template();
-	} // Page
-	elseif ( is_page() ) {
-		get_template_part( 'content', 'page' );
-		comments_template();
-	} // Attachment
-	elseif ( is_attachment() ) {
-		get_template_part( 'content', 'attachment' );
-		comments_template();
-	} // Archive
-	elseif ( is_archive() ) {
+		// Blog
+		if ( is_home() ) {
+			get_template_part( 'content' );
+		} // Post
+		elseif ( is_singular( 'post' ) ) {
+			get_template_part( 'content' );
+			comments_template();
+		} // Page
+		elseif ( is_page() ) {
+			get_template_part( 'content', 'page' );
+			comments_template();
+		} // Attachment
+		elseif ( is_attachment() ) {
+			get_template_part( 'content', 'attachment' );
+			comments_template();
+		} // Archive
+		elseif ( is_archive() ) {
 
-		// check if bbPress is active
-		if ( function_exists( 'is_bbpress' ) ) {
+			// check if bbPress is active
+			if ( function_exists( 'is_bbpress' ) ) {
 
-			// bbPress forum list
-			if ( is_bbpress() ) {
-				get_template_part( 'content/bbpress' );
-			} // normal archive
+				// bbPress forum list
+				if ( is_bbpress() ) {
+					get_template_part( 'content/bbpress' );
+				} // normal archive
+				else {
+					get_template_part( 'content' );
+				}
+			} // Archive
 			else {
 				get_template_part( 'content' );
 			}
-		} // Archive
+		} // bbPress
+		elseif ( function_exists( 'is_bbpress' ) && is_bbpress() ) {
+			get_template_part( 'content/bbpress' );
+		} // Custom Post Type
 		else {
 			get_template_part( 'content' );
 		}
-	} // bbPress
-	elseif ( function_exists( 'is_bbpress' ) && is_bbpress() ) {
-		get_template_part( 'content/bbpress' );
-	} // Custom Post Type
-	else {
-		get_template_part( 'content' );
 	}
 }
 
