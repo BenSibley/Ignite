17ac15e5512238413f3e2ed2ea23c0638dfccad1
BenSibley <ben@competethemes.com>
2016-04-07 15:13:03 -0400

    logo now uses alt text if available

--- logo.php
+++ logo.php
@@ -1,7 +1,13 @@
 <?php
 
-if ( get_theme_mod( 'logo_upload' ) ) {
-	$logo = "<span class='screen-reader-text'>" . get_bloginfo( 'name' ) . "</span><img id='logo' class='logo' src='" . esc_url( get_theme_mod( 'logo_upload' ) ) . "' alt='" . esc_attr( get_bloginfo( 'name' ) ) . "' />";
+$image = get_theme_mod( 'logo_upload' );
+
+if ( $image ) {
+	$image_alt = get_post_meta( attachment_url_to_postid( $image ), '_wp_attachment_image_alt', true);
+	if ( empty( $image_alt ) ) {
+		$image_alt = esc_attr( get_bloginfo( 'name' ) );
+	}
+	$logo = "<span class='screen-reader-text'>" . get_bloginfo( 'name' ) . "</span><img class='logo' src='" . esc_url( get_theme_mod( 'logo_upload' ) ) . "' alt='" . $image_alt . "' />";
 } else {
 	$logo = get_bloginfo( 'name' );
 }
