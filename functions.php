46a8147867d10c2a0979adb2912bd944ea0d4303
BenSibley <ben@competethemes.com>
2018-11-20 09:12:17 -0500

    Gutenberg back-end styles

--- functions.php
+++ functions.php
@@ -55,6 +55,13 @@ if ( ! function_exists( 'ct_ignite_theme_setup' ) ) {
 		add_theme_support( 'wc-product-gallery-zoom' ); 
 		add_theme_support( 'wc-product-gallery-lightbox' ); 
 		add_theme_support( 'wc-product-gallery-slider' );
+
+		// Gutenberg - wide & full images
+		add_theme_support( 'align-wide' );
+		add_theme_support( 'align-full' );
+
+		// Gutenberg - add support for editor styles
+		add_theme_support('editor-styles');
 		
 		load_theme_textdomain( 'ignite', get_template_directory() . '/languages' );
 	}
