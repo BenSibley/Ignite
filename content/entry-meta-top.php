8063a467b0dc31ffaaccd0f928593f99e9f9efa1
BenSibley <ben@competethemes.com>
2019-02-26 10:36:08 -0500

    WP 5.1 date fix

--- content/entry-meta-top.php
+++ content/entry-meta-top.php
@@ -7,7 +7,7 @@ if ( ( $date_display != 'hide' ) || ( $author_display != 'hide' ) ) : ?>
 	<div class="entry-meta-top">
 		<?php
 		if ( $date_display != 'hide' ) {
-			echo esc_html_x( 'Published', 'Published DATE', 'ignite' ) . " " . date_i18n( get_option( 'date_format' ), strtotime( get_the_date( 'r' ) ) );
+			echo esc_html_x( 'Published', 'Published DATE', 'ignite' ) . " " . date_i18n( get_option( 'date_format' ), strtotime( get_the_date( 'c' ) ) );
 		}
 		if ( $author_display != 'hide' ) {
 
