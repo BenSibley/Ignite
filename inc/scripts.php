6a542d3c80d6eb1b4f8b206e35dd98139cbb386a
BenSibley <ben@competethemes.com>
2019-11-25 12:49:52 -0500

    added font-display: swap for Google Fonts

--- inc/scripts.php
+++ inc/scripts.php
@@ -4,8 +4,9 @@
 function ct_ignite_load_scripts_styles() {
 
 	$font_args = array(
-		'family' => urlencode( 'Lusitana:400,700' ),
-		'subset' => urlencode( 'latin,latin-ext' )
+		'family' 	=> urlencode( 'Lusitana:400,700' ),
+		'subset' 	=> urlencode( 'latin,latin-ext' ),
+		'display' => 'swap'
 	);
 	$fonts_url = add_query_arg( $font_args, '//fonts.googleapis.com/css' );
 
