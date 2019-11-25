756876392ba455e211ce4b2517dea81d888d9dcd
BenSibley <ben@competethemes.com>
2019-11-25 13:17:24 -0500

    removed incorrect support function for wide images

--- functions.php
+++ functions.php
@@ -59,7 +59,6 @@ if ( ! function_exists( 'ct_ignite_theme_setup' ) ) {
 
 		// Gutenberg - wide & full images
 		add_theme_support( 'align-wide' );
-		add_theme_support( 'align-full' );
 
 		// Gutenberg - add support for editor styles
 		add_theme_support('editor-styles');
