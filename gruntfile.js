51443313723928c6502cdbdbd0d8b9a9a94299f6
Ben Sibley <ben@competethemes.com>
2020-12-23 11:21:14 -0500

    grunt shell fixes

--- gruntfile.js
+++ gruntfile.js
@@ -138,12 +138,12 @@ module.exports = function(grunt) {
             zip: {
                 command: [
                     // delete existing copies (if they exist)
-                    'rm -R /Users/bensibley/Documents/compete-themes/dist/ignite || true',
-                    'rm -R /Users/bensibley/Documents/compete-themes/dist/ignite.zip || true',
+                    'rm -R "/Users/bensibley/Dropbox/Compete Themes/Distribution/ignite" || true',
+                    'rm -R "/Users/bensibley/Dropbox/Compete Themes/Distribution/ignite.zip" || true',
                     // copy folder without any project/meta files
-                    'rsync -r "/Users/bensibley/Sites/ignite/wp-content/themes/ignite" /Users/bensibley/Documents/compete-themes/dist/ <%= excludeFiles %>',
+                    'rsync -r "/Users/bensibley/Sites/ignite/wp-content/themes/ignite" "/Users/bensibley/Dropbox/Compete Themes/Distribution/" <%= excludeFiles %>',
                     // open dist folder
-                    'cd /Users/bensibley/Documents/compete-themes/dist/',
+                    'cd "/Users/bensibley/Dropbox/Compete Themes/Distribution/"',
                     // zip the ignite folder
                     'zip -r ignite.zip ignite'
                 ].join('&&')
