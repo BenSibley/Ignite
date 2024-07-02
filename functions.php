50b73f6d2c0cefccce02c208d312c75062ced8f2
BenSibley <ben@competethemes.com>
2024-07-02 13:36:08 -0400

    Remove review request

--- functions.php
+++ functions.php
@@ -8,7 +8,6 @@ require_once(trailingslashit(get_template_directory()) . 'inc/breadcrumbs.php');
 require_once(trailingslashit(get_template_directory()) . 'inc/customizer.php');
 require_once(trailingslashit(get_template_directory()) . 'inc/deprecated.php');
 require_once(trailingslashit(get_template_directory()) . 'inc/last-updated-meta-box.php');
-require_once(trailingslashit(get_template_directory()) . 'inc/review.php');
 require_once(trailingslashit(get_template_directory()) . 'inc/scripts.php');
 require_once(trailingslashit(get_template_directory()) . 'inc/widgets/image_widget.php');
 // TGMP
@@ -48,20 +47,6 @@ function ct_ignite_register_required_plugins()
     tgmpa($plugins, $config);
 }
 add_action('tgmpa_register', 'ct_ignite_register_required_plugins');
-
-
-//----------------------------------------------------------------------------------
-//	Include review request
-//----------------------------------------------------------------------------------
-require_once(trailingslashit(get_template_directory()) . 'dnh/handler.php');
-new WP_Review_Me(
-    array(
-        'days_after' => 14,
-        'type'       => 'theme',
-        'slug'       => 'ignite',
-        'message'    => __('Hey! Sorry to interrupt, but you\'ve been using Ignite for a little while now. If you\'re happy with this theme, could you take a minute to leave a review? <i>You won\'t see this notice again after closing it.</i>', 'ignite')
-    )
-);
 
 if (! function_exists(('ct_ignite_set_content_width'))) {
     function ct_ignite_set_content_width()
